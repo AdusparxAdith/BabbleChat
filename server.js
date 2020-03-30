cd779cf671eb7ca036646024bbda0417a535988a
Adusparx <adithdinesh97@gmail.com>
2020-03-31 00:57:39 +0530

    Minor fix, included path

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const cors = require("cors");
+const path = require("path");
 require("./db/db");
 
 const app = express();
